e82d593d697f7558e1e5b271df24087e05d2eb1a
Amanda Giannelli <agiannelli23@gmail.com>
2021-01-02 21:52:05 -0500

    Add in Chuck Norris Joke Generator

--- src/pages/chucknorris.js
+++ src/pages/chucknorris.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { decode } from 'he';
 import Layout from '../components/Layout'
+import logo from '../img/chuck-norris-bg.png'
+import '../app.css';
 
 const ChuckNorris = ({ location }) => {
+
+  const [joke, setJoke] = useState('');
+
+  const fetchJoke = async signal => {
+    const url = new URL('https://api.icndb.com/jokes/random');
+    const response = await fetch(url, { signal });
+    const { value } = await response.json();
+
+    setJoke(decode(value.joke));
+  };
+
+  useEffect(() => {
+    if (!joke) {
+      const controller = new AbortController();
+      fetchJoke(controller.signal);
+
+      return () => controller.abort();
+    }
+  }, [joke]);
+
+
   return (
     <Layout>
-      <h1>Chuck Norris Joke Generator<span>{location.pathname}</span></h1>
-      <h2>Static HTML Page</h2>
+      <img className="logo" src={logo} alt="chuck norris" />
+      <p>{joke || '...'}</p>
+      <button className="link" onClick={() => setJoke('')}>
+        Get a new joke
+        </button>
     </Layout>
   )
 }
